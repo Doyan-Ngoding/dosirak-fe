cbe1443486492e7f8ce3de4e4079031e0b526137
auliyasalsabila <auliyasalsa30@gmail.com>
2025-02-19 13:01:50 +0700

    Config: Responsive Layout

--- src/components/global/layout/index.jsx
+++ src/components/global/layout/index.jsx
@@ -10,6 +10,7 @@ import {
 } from 'antd'
 import { Link, useLocation } from 'react-router-dom';
 import { IconShoppingBag } from '@tabler/icons-react';
+import { useMediaQuery } from 'react-responsive';
 const {
     Header,
     Content,
@@ -29,9 +30,17 @@ export default function LayoutComp(props) {
     const pathname = router.pathname
 
     useEffect(() => {
-        let route = (pathname === "/order-summary" || pathname === "/payment-method") ? 3 : Object.keys(routes).find(key => routes[key] === router.pathname);
+        let route = (pathname === "/order-summary" || pathname === "/payment-method" || pathname === "/complete") ? 3 : Object.keys(routes).find(key => routes[key] === router.pathname);
         setActiveKey(route?.toString());
     }, [pathname]);
+
+    const isStandard = useMediaQuery({ minWidth: 1025 })
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+
+    const setSize = (standard, tablet, mobile) => {
+        return isStandard ? standard : ( isTablet ? tablet : mobile )   
+    }
 
     return (
         <>
@@ -47,22 +56,22 @@ export default function LayoutComp(props) {
                             triggerBg: '#F9F9F9',
                             bodyBg: '#FFFFFF',
                             footerBg: '#FFFFFF',
-                            headerHeight: 76,
+                            headerHeight: setSize(75, 65, 55),
                             footerPadding: '0',
-                            headerPadding: '10px 80px',
+                            headerPadding: setSize('10px 80px', '10px 50px', '10px 30px'), 
                         },
                         Menu: {
                             activeBarHeight: 0,
                             colorSplit: '#C5C5C5',
                             itemPaddingInline: 50,
-                            fontSize: 18,
+                            fontSize: setSize(18, 16, 14),
                             colorText: '#6B6B6B',
                             itemBg: '#F9F9F9',
                         },
                         Button: {
                             controlHeightSM: 26,
                             controlHeightLG: 50,
-                            fontSizeLG: 16,
+                            fontSize: setSize(16, 12, 12),
                         },
                         Select: {
                             colorBgContainer: '#FA5523',
@@ -87,7 +96,7 @@ export default function LayoutComp(props) {
                             bodyPadding: '0.5em 1em'
                         },
                         Input: {
-                            fontSize: 16,
+                            fontSize: setSize(16, 12, 12),
                             controlHeightLG: 80,
                         },
                     }
@@ -95,38 +104,23 @@ export default function LayoutComp(props) {
             >
                 <Layout>
                     <Header
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            borderBottom: '1px solid #C5C5C5',
-                            zIndex: 1,
-                            position: 'sticky',
-                            top: 0,
-                        }}
+                        className='flex items-center border-b border-[#C5C5C5] z-1 sticky top-0'
                     >
-                        <div
-                            style={{
-                                backgroundColor: '#E83600',
-                                height: '50%',
-                                borderRadius: 4,
-                                width: '6%'
-                            }}
-                        >
-                        </div>
+                        {
+                            !isMobile && (
+                                <div
+                                    className='h-[50%] rounded-sm bg-[#E83600] lg:w-[8%] md:w-[10%] sm:w-[10%] w-[15%]'
+                                >
+                                </div>
+                            )
+                        }
                         <Menu
                             theme='light'
                             mode='horizontal'
                             selectedKeys={[activeKey]} 
                             defaultSelectedKeys={['1']} 
                             onClick={e => setActiveKey(e)}
-                            style={{
-                                flex: 1,
-                                minWidth: 0,
-                                position: 'relative',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                fontWeight: 600,
-                            }}
+                            className='felx flex-1 justify-center min-w-0 relative font-semibold'
                         >
                             <Menu.Item
                                 key={'1'}
@@ -165,17 +159,12 @@ export default function LayoutComp(props) {
                                 </Link>
                             </Menu.Item>
                         </Menu>  
-                        <div
-                            style={{
-                                display: 'flex',
-                                alignItems: 'center'
-                            }}
+                        <div 
+                            className='flex items-center'
                         >
                             <IconShoppingBag 
                                 color='#6B6B6B'
-                                style={{
-                                    marginRight: 20
-                                }}
+                                className='mr-5'
                             />
                             <Button
                                 type='primary'
@@ -186,163 +175,168 @@ export default function LayoutComp(props) {
                     </Header>
                     <Content>
                         <div
-                            style={{
-                                backgroundColor: '#FFFFFF',
-                            }}
+                            className='bg-white'
                         >
                             {props.children}
                         </div>
                     </Content>
                     <Footer
-                        style={{
-                            fontSize: 16
-                        }}
+                        className='lg:text-[14px] md:text-[12px] text-[10px]'
                     >
-                        <div
+                        <Row
+                            justify={"space-between"}
+                            align={"top"}
                             style={{
-                                padding: '30px 80px',
-                                display: 'flex',
-                                justifyContent: 'space-between'
+                                padding: setSize('30px 80px', '30px 50px', '30px 30px'),
+                                width: "100%"
                             }}
                         >
-                            <div
-                                style={{
-                                    width: '30%'
-                                }}
+                            {
+                                isMobile && (
+                                    <Col
+                                        span={setSize(8, 8, 24)}
+                                        className='p-10'
+                                    >
+                                        <div
+                                            className='text-[#6B6B6B] text-center font-medium pb-7'
+                                        >
+                                            Get Exclusive Deals in your Inbox
+                                        </div>
+                                        <div>
+                                            <Input 
+                                                placeholder='Insert your email here (e.g youremail@gmail.com)'
+                                                suffix={
+                                                    <Button
+                                                        size='small'
+                                                        type='primary'
+                                                        style={{
+                                                            borderRadius: 50
+                                                        }}
+                                                        className='rounded-[50]'
+                                                    >
+                                                        Subscribe
+                                                    </Button>
+                                                }
+                                                style={{
+                                                    borderRadius: 50,
+                                                }}
+                                            />
+                                        </div>
+                                    </Col>
+                                )
+                            }
+                            <Col
+                                span={setSize(8, 8, 12)}
                             >
                                 <div
-                                    style={{
-                                        backgroundColor: '#DCDCDC',
-                                        height: 110,
-                                        width: 350,
-                                        marginBottom: 20
-                                    }}
+                                    className="h-[100px] lg:h-[120px] lg:w-[80%] md:w-[80%] w-[100%] bg-[#DCDCDC]"
                                 />
-                                <div>
+                                <div
+                                    className='lg:text-[14px] md:text-[12px] text-[10px]'
+                                >
                                     Company # 490039-445, Registered with <br></br> House of companies.
                                 </div>
-                            </div>
-                            <div
-                                style={{
-                                    width: '50%',
-                                    paddingLeft: 100
-                                }}
-                            >
-                                <div
-                                    style={{
-                                        fontSize: 20,
-                                        fontWeight: 600,
-                                        color: '#6B6B6B',
-                                        paddingBottom: 30
-                                    }}
-                                >
-                                    Get Exclusive Deals in your Inbox
-                                </div>
-                                <div>
-                                    <Input 
-                                        placeholder='Insert your email here (e.g youremail@gmail.com)'
-                                        suffix={
-                                            <Button
-                                                type='primary'
+                            </Col>
+                            {
+                                !isMobile && (
+                                    <Col
+                                        span={setSize(8, 10, 12)}
+                                        className='px-5'
+                                    >
+                                        <div
+                                            className='text-[#6B6B6B] text-center lg:font-bold md:font-semibold lg:pb-12 pb-7 md:text-[16px] lg:text-[20px]'
+                                        >
+                                            Get Exclusive Deals in your Inbox
+                                        </div>
+                                        <div>
+                                            <Input 
+                                                placeholder='Insert your email here (e.g youremail@gmail.com)'
+                                                suffix={
+                                                    <Button
+                                                        size={setSize('medium', 'small', '')}
+                                                        type='primary'
+                                                        style={{
+                                                            borderRadius: 50
+                                                        }}
+                                                    >
+                                                        Subscribe
+                                                    </Button>
+                                                }
                                                 style={{
-                                                    borderRadius: 50
+                                                    borderRadius: 50,
+                                                    padding: 10
                                                 }}
-                                            >
-                                                Subscribe
-                                            </Button>
-                                        }
-                                        style={{
-                                            width: '65%',
-                                            borderRadius: 50,
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                            <div
-                                style={{
-                                    width: '20%'
-                                }}
+                                            />
+                                        </div>
+                                    </Col>
+                                    
+                                )
+                            }
+                            <Col
+                                span={setSize(6, 6, 12)}
+                                className='lg:pl-20 md:pl-0 pl-10'
                             >
-                                <div
-                                    style={{
-                                        fontWeight: 600,
-                                        fontSize: 20,
-                                        color: '#6B6B6B'
-                                    }}
+                                 <div
+                                    className='text-[#6B6B6B] lg:font-bold font-semibold lg:text-lg md:text-[15px] text-sm'
                                 >
                                     Legal Page
                                 </div>
                                 <div
-                                    style={{
-                                        color: '#393939',
-                                        paddingTop: 20
-                                    }}
+                                    className='text-[#393939] lg:pt-5 md:pt-4 pt-2.5 lg:text-[14px] md:text-[14px] text-[12px]'
                                 >
                                     Terms and Condition
                                 </div>
                                 <div
-                                   style={{
-                                        color: '#393939',
-                                        paddingTop: 20
-                                    }}
+                                    className='text-[#393939] lg:pt-3 md:pt-2 pt-1 lg:text-[14px] md:text-[14px] text-[12px]'
                                 >
                                     Privacy
                                 </div>
                                 <div
-                                    style={{
-                                        color: '#393939',
-                                        paddingTop: 20,
-                                    }}
+                                    className='text-[#393939] lg:pt-3 md:pt-2 pt-1 lg:text-[14px] md:text-[14px] text-[12px]'
                                 >
                                     Cookies
                                 </div>
                                 <div
-                                    style={{
-                                        color: '#393939',
-                                        paddingTop: 20,
-                                    }}
+                                    className='text-[#393939] lg:pt-3 md:pt-2 pt-1 lg:text-[14px] md:text-[14px] text-[12px]'
                                 >
                                     Modern Slavery Statement
                                 </div>
-                            </div>
-                        </div>
-                        <div
+                            </Col>
+                        </Row>
+                        <Row
+                            justify={setSize("space-between", "space-between", "center")}
+                            align={"top"}
                             style={{
+                                padding: setSize('10px 80px', '10px 50px', '10px 30px'),
+                                width: "100%",
                                 backgroundColor: '#6B6B6B',
                                 color: '#FFFFFF',
-                                padding: '15px 80px',
-                                display: 'flex',
-                                justifyContent: 'space-between'
                             }}
                         >
-                            <div>
+                            <Col
+                                span={setSize(12, 12, 24)}
+                                className='lg:text-left md:text-left text-center'
+                            >
                                 Dosirak Copyright 2025, All Rights Reserved.
-                            </div>
-                            <div
-                                style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between'
-                                }}
+                            </Col>
+                            <Col 
+                                className='flex'
                             >
                                 <div
-                                    style={{
-                                        paddingRight: 50
-                                    }}
+                                    className='pr-10'
                                 >
                                     Privacy Policy
                                 </div>
                                 <div
-                                    style={{
-                                        paddingRight: 50
-                                    }}
+                                    className='pr-10'
                                 >
                                     Term
                                 </div>
                                 <div>
                                     Pricing
                                 </div>
-                            </div>
-                        </div>
+                            </Col>
+                        </Row>
                     </Footer>
                 </Layout>
             </ConfigProvider>
