3ff288d74624090ed61e399fd04b6a5ddc541e29
auliyasalsabila <auliyasalsa30@gmail.com>
2025-03-01 19:20:10 +0700

    Update: config

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,18 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('lodash')) return 'lodash-vendor';
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
   server: {
     port: 8080,
